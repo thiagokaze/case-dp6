41c91c7953f32f7bd43554e98f6c245eaa27cc03
thiagokaze <tesp78@gmail.com>
2021-07-05 10:11:47 -0300

    Adição de Validação de Conteudo do Form.

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -5,10 +5,17 @@
 // para fazer a sua coleta.
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', 'UA-12345-6', 'auto');
 
@@ -25,8 +32,8 @@ var analise_link = document.getElementsByClassName("card-montadoras");
 var form = document.getElementsByTagName("form");
 
 //imaginando o reuso do codigo para mais links com a mesma classe
-for(var item of contato_link){
-  item.addEventListener('click', function(event){
+for (var item of contato_link) {
+  item.addEventListener('click', function(event) {
     ga('send', {
       hitType: 'event',
       eventCategory: 'menu',
@@ -36,8 +43,8 @@ for(var item of contato_link){
   });
 }
 
-for(var item of download_link){
-  item.addEventListener('click', function(event){
+for (var item of download_link) {
+  item.addEventListener('click', function(event) {
     ga('send', {
       hitType: 'event',
       eventCategory: 'menu',
@@ -47,8 +54,8 @@ for(var item of download_link){
   });
 }
 
-for(var item of analise_link){
-  item.addEventListener('click', function(event){
+for (var item of analise_link) {
+  item.addEventListener('click', function(event) {
 
     ga('send', {
       hitType: 'event',
@@ -60,41 +67,87 @@ for(var item of analise_link){
 }
 
 
-  var form_fields = form[0].getElementsByTagName("input");
-
-  for(var item of form_fields){
-    item.addEventListener('focusout', function(event, item){
-      if(this.value.trim() !== ""){
-        ga('send', {
-          hitType: 'event',
-          eventCategory: 'contato',
-          eventAction: this.id,
-          eventLabel: 'preencheu'
-        });
+var form_fields = form[0].getElementsByTagName("input");
+/*Foram feitas checagens extras para não popular o GA com dados inválidos*/
+for (var item of form_fields) {
+  item.addEventListener('focusout', function(event, item) {
+    if (this.value.trim() !== "") {
+      if (this.id == 'email') {
+        if (validateEmail(this.value)) {
+          ga('send', {
+            hitType: 'event',
+            eventCategory: 'contato',
+            eventAction: this.id,
+            eventLabel: 'preencheu'
+          });
+        }
+      } else if (this.id == 'telefone') {
+        console.log(validatePhone(this.value))
+        if (validatePhone(this.value)) {
+          ga('send', {
+            hitType: 'event',
+            eventCategory: 'contato',
+            eventAction: this.id,
+            eventLabel: 'preencheu'
+          });
+        }
+      } else if (this.id == 'aceito') {
+        if (this.checked == true) {
+          ga('send', {
+            hitType: 'event',
+            eventCategory: 'contato',
+            eventAction: this.id,
+            eventLabel: 'preencheu'
+          });
+        }
+      } else {
+        if (this.value.length > 4) {
+          ga('send', {
+            hitType: 'event',
+            eventCategory: 'contato',
+            eventAction: this.id,
+            eventLabel: 'preencheu'
+          });
+        }
       }
-    });
-  }
+
+
+    }
+  });
+}
 
 //observer para a mudanca de class do body
 var body_tag = document.body;
-const config = { attributes: true}
+const config = {
+  attributes: true
+}
 const callback = function(mutationsList, observer) {
-    for(const mutation of mutationsList) {
-       if (mutation.type === 'attributes') {
-         var class_array = mutation.target.className.split(" ") ;
-         const found = class_array.includes('lightbox-open');
-            if(found == true){
-              ga('send', {
-                hitType: 'event',
-                eventCategory: 'contato',
-                eventAction: 'enviado',
-                eventLabel: 'enviado'
-              });
+  for (const mutation of mutationsList) {
+    if (mutation.type === 'attributes') {
+      var class_array = mutation.target.className.split(" ");
+      const found = class_array.includes('lightbox-open');
+      if (found == true) {
+        ga('send', {
+          hitType: 'event',
+          eventCategory: 'contato',
+          eventAction: 'enviado',
+          eventLabel: 'enviado'
+        });
 
-            }
+      }
 
-        }
     }
+  }
 };
 const watcher = new MutationObserver(callback);
 watcher.observe(body_tag, config);
+
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function validatePhone(phone) {
+  const re = /^[(]?[0-9]{2}[)]?[-\s]?[0-9]{4,5}[-\s]?[0-9]{4}$/im;
+  return re.test(String(phone));
+}
